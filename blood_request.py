27ac157c3f66541a970f90ad1244837ff11a078a
shadatm <shadat.rahman.464@gmail.com>
2022-07-31 18:49:17 +0600

    blood request CRUD done

--- blood_request.py
+++ blood_request.py
@@ -1,3 +1,4 @@
+from pickletools import read_uint1
 from fastapi import Depends, FastAPI, APIRouter, Response, status, HTTPException
 from typing import List
 from sqlalchemy.orm import Session
@@ -12,7 +13,45 @@ router = APIRouter(
 )
 
 
-@router.get("/", response_model=List[schemas.BloodRequest])
+@router.get("/", response_model=List[schemas.BloodRequest], status_code=status.HTTP_200_OK)
 def get_all_requests(db: Session = Depends(get_db)):
     all_requests = db.query(models.BloodRequest).all()
     return all_requests
+
+
+@router.post("/", response_model=schemas.BloodRequest, status_code=status.HTTP_201_CREATED)
+def create_blood_request(request: schemas.CreateBloodRequest, db: Session = Depends(get_db)):
+    new_request = models.BloodRequest(**request.dict())
+    db.add(new_request)
+    db.commit()
+    db.refresh(new_request)
+    return new_request
+
+
+@router.put('/{id}',  response_model=schemas.BloodRequest, status_code=status.HTTP_200_OK)
+def update_blood_request(id: int, request: schemas.UpdateBloodRequest, db: Session = Depends(get_db)):
+    req_query = db.query(models.BloodRequest).filter(
+        models.BloodRequest.id == id)
+    req = req_query.first()
+
+    if req == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blood request with id: {id} not found")
+
+    req_query.update(request.dict(), synchronize_session=False)
+    db.commit()
+    return req_query.first()
+
+
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_blood_request(id: int, db: Session = Depends(get_db)):
+    req_query = db.query(models.BloodRequest).filter(
+        models.BloodRequest.id == id)
+    request = req_query.first()
+    if request == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blood request with id: {id} not found")
+
+    req_query.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
--- schemas.py
+++ schemas.py
@@ -6,6 +6,7 @@ from pydantic import BaseModel
 
 
 class BloodRequest(BaseModel):
+    id: int
     posted_by: str
     donation_time: datetime
     blood_group: str
@@ -15,6 +16,33 @@ class BloodRequest(BaseModel):
     contact_number: str
     urgent: bool
     created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class CreateBloodRequest(BaseModel):
+    posted_by: str
+    donation_time: datetime
+    blood_group: str
+    bag_needed: int
+    blood_type: str
+    hospital_name: str
+    contact_number: str
+    urgent: bool
+
+    class Config:
+        orm_mode = True
+
+
+class UpdateBloodRequest(BaseModel):
+    donation_time: datetime
+    blood_group: str
+    bag_needed: int
+    blood_type: str
+    hospital_name: str
+    contact_number: str
+    urgent: bool
 
     class Config:
         orm_mode = True
